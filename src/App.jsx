4a80eeb07c14c8dccab12da78c4710eee263add4
crossroads30 <crossroads30@mail.ru>
2024-01-15 18:22:25 +0300

    feat: set a page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	BrowserRouter,
+} from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {
+	About,
+	Cart,
+	Checkout,
+	Error,
+	Home,
+	Products,
+	SingleProduct,
+	PrivateRoute,
+} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/cart' element={<Cart />} />
+				<Route path='/products' element={<Products />} />
+				<Route path='/products/:id' element={<SingleProduct />} />
+				<Route path='/checkout' element={<Checkout />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+			<Sidebar />
+			<Footer />
+		</BrowserRouter>
+	)
 }
 
 export default App
