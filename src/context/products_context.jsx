23a44ad03575ed9cb0dbf6adf459bc31fe276ec9
crossroads30 <crossroads30@mail.ru>
2024-01-16 16:59:42 +0300

    feat: add fetch func for single products url with dispatches

--- src/context/products_context.jsx
+++ src/context/products_context.jsx
@@ -19,6 +19,9 @@ const initialState = {
 	products_error: false,
 	products: [],
 	featured_products: [],
+	single_product_loading: false,
+	single_product_error: false,
+	single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -38,10 +41,21 @@ export const ProductsProvider = ({ children }) => {
 		try {
 			const response = await axios.get(url)
 			const products = response.data
-			console.log(products)
+			// console.log(products)
 			dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
 		} catch (error) {
 			dispatch({ type: GET_PRODUCTS_ERROR })
+		}
+	}
+
+	const fetchSingleProduct = async url => {
+		dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+		try {
+			const response = await axios.get(url)
+			const single_product = response.data
+			dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: single_product })
+		} catch (error) {
+			dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
 		}
 	}
 
