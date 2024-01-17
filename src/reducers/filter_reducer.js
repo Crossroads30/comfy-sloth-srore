9dd9daec1a34f3f4540be1257dfcb8e4da15b224
crossroads30 <crossroads30@mail.ru>
2024-01-17 21:21:20 +0300

    feat: add action

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,17 +1,25 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
+	LOAD_PRODUCTS,
+	SET_LISTVIEW,
+	SET_GRIDVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
 } from '../actions'
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
+	if (action.type === LOAD_PRODUCTS) {
+		return {
+			...state,
+      // we must use here spread op., course we use the same payload in two different values, otherwise, we wont be able to go back from 'filtered_prod' to 'all_prod', it is the way the JS works with memory with the point in the same place in the memory!!!  
+			all_products: [...action.payload],
+			filtered_products: [...action.payload],
+		}
+	}
+
+	throw new Error(`No Matching "${action.type}" - action type`)
 }
 
 export default filter_reducer
