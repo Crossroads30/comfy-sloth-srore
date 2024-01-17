abe5c44e465ed29ba53e014e2c9552f3493cc308
crossroads30 <crossroads30@mail.ru>
2024-01-17 13:53:25 +0300

    feat: add cond for singleProductPage

--- src/components/PageHero.jsx
+++ src/components/PageHero.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const PageHero = ({title}) => {
-  return (
+const PageHero = ({ title, product }) => {
+	return (
 		<Wrapper>
 			<div className='section-center'>
 				<h3>
-					<Link to='/'>Home</Link>/ {title}
+					<Link to='/'>Home</Link>
+					{product && <Link to='/product'>/ Products</Link>}/ {title}
 				</h3>
 			</div>
 		</Wrapper>
@@ -15,21 +16,21 @@ const PageHero = ({title}) => {
 }
 
 const Wrapper = styled.section`
-  background: var(--clr-primary-10);
-  width: 100%;
-  min-height: 20vh;
-  display: flex;
-  align-items: center;
+	background: var(--clr-primary-10);
+	width: 100%;
+	min-height: 20vh;
+	display: flex;
+	align-items: center;
 
-  color: var(--clr-primary-1);
-  a {
-    color: var(--clr-primary-3);
-    padding: 0.5rem;
-    transition: var(--transition);
-  }
-  a:hover {
-    color: var(--clr-primary-1);
-  }
+	color: var(--clr-primary-1);
+	a {
+		color: var(--clr-primary-3);
+		padding: 0.5rem;
+		transition: var(--transition);
+	}
+	a:hover {
+		color: var(--clr-primary-1);
+	}
 `
 
 export default PageHero
