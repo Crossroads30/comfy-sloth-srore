0363f53b2353f045600845f3e840db6cb324fc33
crossroads30 <crossroads30@mail.ru>
2024-01-18 13:12:35 +0300

    feast: add actions

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,9 +13,21 @@ const filter_reducer = (state, action) => {
 	if (action.type === LOAD_PRODUCTS) {
 		return {
 			...state,
-      // we must use here spread op., course we use the same payload in two different values, otherwise, we wont be able to go back from 'filtered_prod' to 'all_prod', it is the way the JS works with memory with the point in the same place in the memory!!!  
+			// we must use here spread op., course we use the same payload in two different values, otherwise, we wont be able to go back from 'filtered_prod' to 'all_prod', it is the way the JS works with memory with the point in the same place in the memory!!!
 			all_products: [...action.payload],
 			filtered_products: [...action.payload],
+		}
+	}
+	if (action.type === SET_GRIDVIEW) {
+		return {
+			...state,
+			grid_view: true,
+		}
+	}
+	if (action.type === SET_LISTVIEW) {
+		return {
+			...state,
+			grid_view: false,
 		}
 	}
 
