585729c3a770dc8f817bb063917f897108b16587
crossroads30 <crossroads30@mail.ru>
2024-01-20 12:58:11 +0300

    feat: add auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,32 @@ import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
+import AuthProvider from 'react-auth-kit/AuthProvider'
 import { Auth0Provider } from '@auth0/auth0-react'
+// import createStore from 'react-auth-kit/createStore'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+// const store = createStore({
+// 	authName: '_auth',
+// 	authType: 'cookie',
+// 	cookieDomain: window.location.hostname,
+// 	cookieSecure: window.location.protocol === 'https:',
+// })
+
 root.render(
-	<ProductsProvider>
-		<FilterProvider>
-			<CartProvider>
-				<App />
-			</CartProvider>
-		</FilterProvider>
-	</ProductsProvider>
+	<Auth0Provider
+		domain='dev-pu8wyk-g.us.ayth0.com'
+		clientId='7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu'
+		redirec_uri={window.location.origin}
+		cacheLocation='localstorage'
+	>
+		<ProductsProvider>
+			<FilterProvider>
+				<CartProvider>
+					<App />
+				</CartProvider>
+			</FilterProvider>
+		</ProductsProvider>
+	</Auth0Provider>
 )
