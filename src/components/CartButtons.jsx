08eb2eae476903c606207afd140fc91687283bce
crossroads30 <crossroads30@mail.ru>
2024-01-21 14:18:30 +0300

    feat: add clear cart when logout

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
 	const { closeSidebar } = useProductsContext()
-	const { total_items } = useCartContext()
+	const { total_items, clearCart } = useCartContext()
 	const {
 		loginWithRedirect,
 		logout,
@@ -32,7 +32,10 @@ const CartButtons = () => {
 					type='button'
 					className='auth-btn'
 					// onClick={() => logout({ returnTo: window.location.origin })}
-					onClick={logoutUser}
+					onClick={() => {
+						clearCart()
+						logoutUser()
+					}}
 				>
 					Logout <FaUserMinus />
 				</button>
