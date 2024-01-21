b0a1051311598c6bc7f45d58e3da5c8df1111024
crossroads30 <crossroads30@mail.ru>
2024-01-22 01:46:44 +0300

    fix: comment unused feature

--- src/context/user_context.jsx
+++ src/context/user_context.jsx
@@ -3,38 +3,61 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
-	const { loginWithRedirect, logout, user } = useAuth0()
+	// const { loginWithRedirect, logout, user } = useAuth0()
 
-	const [myUser, setMyUser] = useState(null)
+	// const [myUser, setMyUser] = useState(null)
 	const [authenticated, setAuthenticated] = useState(false)
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 	const [loginEmail, setLoginEmail] = useState('')
 	const [loginPassword, setLoginPassword] = useState('')
+	const [submit, setSubmit] = useState(false)
 	const [alert, setAlert] = useState({
 		show: false,
 		msg: '',
 	})
 
 	useEffect(() => {
-		if (loginEmail === 'comfyTest@email.com' && loginPassword === '12345') {
+		if (
+			submit &&
+			loginEmail === 'comfyTest@email.com' &&
+			loginPassword === '12345'
+		) {
 			setAuthenticated(true)
+			setSubmit(false)
 		}
-		setAlert(true)
-		showAlert(true, 'Wrong email or password!')
-	}, [loginEmail, loginPassword])
+		if (
+			(submit &&
+				loginEmail !== 'comfyTest@email.com' &&
+				loginPassword !== '12345') ||
+			(submit && loginEmail !== 'comfyTest@email.com') ||
+			(submit && loginPassword !== '12345')
+		) {
+			showAlert(true, 'Wrong email or password!')
+			setSubmit(false)
+		}
+
+		if (
+			(submit && !loginEmail && !loginPassword) ||
+			(submit && !loginEmail) ||
+			(submit && !loginPassword)
+		) {
+			showAlert(true, "Email or password can't be empty!")
+			setSubmit(false)
+		}
+	}, [submit])
 
 	const handelSubmit = e => {
 		e.preventDefault()
-		console.log('submit')
 		setLoginEmail(email)
 		setLoginPassword(password)
 		setEmail('')
 		setPassword('')
+		setSubmit(true)
 	}
 
 	const showAlert = (show = false, msg = '') => {
-		setAlert({show,msg})
+		setAlert({ show, msg })
 	}
 
 	// for Auth0
@@ -49,20 +72,20 @@ export const UserProvider = ({ children }) => {
 		setAuthenticated(false)
 	}
 
-	useEffect(() => {
-		if (authenticated) {
-			setMyUser(true)
-		} else {
-			setMyUser(false)
-		}
-	}, [authenticated])
+	// useEffect(() => {
+	// 	if (authenticated) {
+	// 		setMyUser(true)
+	// 	} else {
+	// 		setMyUser(false)
+	// 	}
+	// }, [authenticated])
 
 	return (
 		<UserContext.Provider
 			value={{
-				loginWithRedirect,
-				logout,
-				myUser,
+				// loginWithRedirect,
+				// logout,
+				// myUser,
 				loginUser,
 				logoutUser,
 				authenticated,
