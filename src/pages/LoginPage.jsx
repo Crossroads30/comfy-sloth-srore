e9a9163476aa6b78367e09d271f82217a19a64cb
crossroads30 <crossroads30@mail.ru>
2024-01-22 00:56:11 +0300

    feat: add alert add props

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,31 @@
 import styled from 'styled-components'
 import { useUserContext } from '../context/user_context'
+import { Navigate } from 'react-router-dom'
+import Alert from '../components/Alert'
 
 const LoginPage = () => {
-	const { email, password, setEmail, setPassword, handelSubmit } =
-		useUserContext()
+	const {
+		email,
+		password,
+		setEmail,
+		setPassword,
+		handelSubmit,
+		loginEmail,
+		loginPassword,
+		authenticated,
+		alert,
+		showAlert,
+	} = useUserContext()
+
+
+	if (authenticated) {
+		return <Navigate to='/' />
+	}
 	return (
 		<Wrapper className='page-100 section'>
 			<div className='login'>
-				<h3>Please login</h3>
+				{alert.show && <Alert showAlert={showAlert} />}
+				<h3>Enter your login</h3>
 				<form onSubmit={handelSubmit} className='form-control'>
 					<input
 						value={email}
@@ -27,6 +45,12 @@ const LoginPage = () => {
 						login
 					</button>
 				</form>
+				<p>
+					Test email : <span>comfyTest@email.com</span>{' '}
+				</p>
+				<p>
+					Test password : <span>12345</span>{' '}
+				</p>
 			</div>
 		</Wrapper>
 	)
@@ -40,18 +64,18 @@ const Wrapper = styled.main`
 	text-align: center;
 
 	.login {
-		border-radius: 5px;
-		padding: 10px;
 	}
 	h3 {
 		font-size: 1.2rem;
 		text-transform: none;
 		margin-bottom: 0.5rem;
+		color: var(--clr-primary-5);
 	}
 	.form-control {
 		display: flex;
 		flex-direction: column;
 		max-width: 300px;
+		margin-bottom: 1rem;
 		input {
 			padding: 0.5rem;
 			background: var(--clr-grey-10);
@@ -60,6 +84,15 @@ const Wrapper = styled.main`
 			letter-spacing: var(--spacing);
 			margin-bottom: 1.5rem;
 		}
+	}
+	p {
+		color: var(--clr-primary-5);
+		span {
+			color: green;
+		}
+	}
+	.alert {
+		color: red;
 	}
 `
 
