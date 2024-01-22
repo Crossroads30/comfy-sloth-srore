fbcf206691180628879abbf52d0cc8af0529d8c0
crossroads30 <crossroads30@mail.ru>
2024-01-22 14:50:09 +0300

    feat: add fake payment method

--- src/components/StripeCheckout.jsx
+++ src/components/StripeCheckout.jsx
@@ -15,14 +15,140 @@ import { useNavigate } from 'react-router-dom'
 
 // const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
 
+const stripePromise = loadStripe(
+	'pk_test_51Oaz6oI0ut9kAUXffrX9zXyTorr1bIsczXlRw87AwxduxVERAUlV99gtHygP34q2GJKVFzki8PS33c59ab1elhD200thSWULl2'
+)
+
 const CheckoutForm = () => {
-	return <h4>hello from Stripe Checkout </h4>
+	const { cart, total_amount, shipping_fee, clearCart } = useCartContext()
+	// const{myUser} = useUserContext()
+	const navigate = useNavigate()
+
+	// STRIPE STUFF:
+	const [succeeded, setSucceeded] = useState(false)
+	const [error, setError] = useState(null)
+	const [processing, setProcessing] = useState('')
+	const [disabled, setDisabled] = useState(true)
+	const [clientSecret, setClientSecret] = useState('')
+	const stripe = useStripe()
+	const elements = useElements()
+
+	const cardStyle = {
+		style: {
+			base: {
+				color: '#32325d',
+				fontFamily: 'Arial, sans-serif',
+				fontSmoothing: 'antialiased',
+				fontSize: '16px',
+				'::placeholder': {
+					color: '#32325d',
+				},
+			},
+			invalid: {
+				color: '#fa755a',
+				iconColor: '#fa755a',
+			},
+		},
+	}
+
+	// for netlify server
+	// const createPaymentIntent = async () => {
+	// 	try {
+	// 		const { data } = await axios.post(
+	// 			'/.netlify/functions/create-payment-intent',
+	// 			JSON.stringify(cart, shipping_fee, total_amount)
+	// 		)
+	// 		setClientSecret(data.clientSecret)
+	// 	} catch (error) {
+	// 		return {
+	// 			stateCode: 500,
+	// 			body: JSON.stringify({ error: error.message }),
+	// 		}
+	// 	}
+	// }
+
+	// useEffect(() => {
+	// 	createPaymentIntent()
+	// 	//eslint-disable-next-line
+	// }, [])
+
+	const handleChange = async event => {
+		setDisabled(event.empty)
+		setError(event.error ? event.error.message : '')
+	}
+
+	const handleSubmit = async event => {
+		event.preventDefault()
+		if (!error) {
+			setProcessing(true)
+			// const payload = await stripe.confirmCardPayment(clientSecret, {
+			// 	payment_method: {
+			// 		card: elements.getElement(CardElement),
+			// 	},
+			// })
+			// if (payload.error) {
+			// 	setError(`Payment failed ${payload.error.message}`)
+			// 	setProcessing(false)
+			// } else {
+				setError(null)
+				setProcessing(false)
+				setSucceeded(true)
+				setTimeout(() => {
+					clearCart()
+					navigate('/')
+				}, 10000)
+			// }
+		}
+		return
+	}
+
+	return (
+		<div>
+			{succeeded ? (
+				<article>
+					<h4>Thank you</h4>
+					<h4>Your payment was successful!</h4>
+					<h4>Redirecting to home page short</h4>
+				</article>
+			) : (
+				<article>
+					<h4>Hello,</h4>
+					<p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+					<p>Test Card Number: 4242 4242 4242 4242 </p>
+				</article>
+			)}
+			<form id='payment-form' onSubmit={handleSubmit}>
+				<CardElement
+					id='card-element'
+					option={cardStyle}
+					onChange={handleChange}
+				/>
+				<button disabled={processing || disabled || succeeded} id='submit'>
+					<span id='button-text'>
+						{processing ? <div className='spinner' id='spinner'></div> : 'Pay'}
+					</span>
+				</button>
+				{/* Show any error that happens when processing the payment */}
+				{error && (
+					<div className='card-error' role='alert'>
+						{error}
+					</div>
+				)}
+				{/* Show a success message upon completion */}
+				<p className={succeeded ? 'result-message' : 'result-message hidden'}>
+					Payment succeeded, see the result in your{' '}
+					<a href={`https://dashboard.stripe.com/test`}>Stripe dashboard</a>
+					Refresh the page to pay again
+				</p>
+			</form>
+		</div>
+	)
 }
 
 const StripeCheckout = () => {
 	return (
 		<Wrapper>
-			<Elements stripe={promise}>
+			<Elements stripe={stripePromise}>
 				<CheckoutForm />
 			</Elements>
 		</Wrapper>
@@ -32,6 +158,7 @@ const StripeCheckout = () => {
 const Wrapper = styled.section`
 	form {
 		width: 30vw;
+		min-width: 500px;
 		align-self: center;
 		box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
 			0px 2px 5px 0px rgba(50, 50, 93, 0.1),
